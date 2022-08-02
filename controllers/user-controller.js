0e8472fdd49cea20c2ea321100069f006b67d219
zacharyrb99 <zach.boudreaux999@gmail.com>
2022-08-02 00:15:44 -0400

    fixed user-controller typo, added .gitignore

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,7 +25,7 @@ loginUser = (req, res) => {
       console.log("user", user);
       if (user) {
         if (user.validPassword(req.body.password)) {
-          response.status(200).json({"Log in successful": user});
+          res.status(200).json({"Log in successful": user});
         } else {
           res.status(400).json("Invalid password");
         }
